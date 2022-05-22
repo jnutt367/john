9b64ed02582b2c37ecc06a4c05ec777eb75110bc
Jason Nutt <jnutt367@gmail.com>
2022-05-21 20:46:19 -0400

    commit for caps title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          the Gospel of <a href="#"> John</a>
+          The Gospel of <a href="#"> John</a>
         </h1>
 
         <p className={styles.description}>
