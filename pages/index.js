fd3c03e3912913fd534607015bab459b78da8ea7
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-17 23:23:35 -0500

    Update index.js
    mary-mother-jesus-john-beloved-disciple-mary-magdalene-crucifixion_24381-1879.webp

--- pages/index.js
+++ pages/index.js
@@ -18,13 +18,13 @@ export default function Home() {
         </h1>
          <div className={styles.hero}>
        <Image 
-                                src="/trust.webp"
+                                src="/mary-mother-jesus-john-beloved-disciple-mary-magdalene-crucifixion_24381-1879.webp"
                                 alt="john image"
                                 height={900}
                                 width={2000}
                                 priority
                               />
-                          
+               <div className={styles.nav}>           
        <a href="https://the-holy-bible.vercel.app/">
             <button className={styles.button}> HOME </button>
        </a>
@@ -38,6 +38,7 @@ export default function Home() {
         <a href="https://acts-zeta.vercel.app/">
             <button className={styles.button}>NEXT > </button>
        </a>           
+            </div>
         <div className={styles.grid}>
         <div className={styles.card}>
             <h2>Chapter 1 &darr;</h2>
