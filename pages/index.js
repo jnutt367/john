cb91b7d1606043f7d738dba6690a68a5b2e6e560
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-04-04 08:52:41 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -48,8 +48,8 @@ export default function Home() {
          
              <p className={styles.donate}>
                 <a href="https://gofund.me/a2b590af">
-               Support <br />
-                  Creator
+                  <br />
+               GIVE <br />
                 </a>
                </p>
        
