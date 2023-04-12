50666b955f5d3d5a07765ccf5c2c1cf9e9ffdf9e
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-04-12 16:17:46 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,6 +33,7 @@ export default function Home() {
                                       width={2000}
                                       priority
                                     />
+                                        <iframe width="100%" height="175" src="https://www.youtube.com/embed/zr54VtQNbgg?autoplay=1&mute=1" title="YouTube video player" frameborder="5" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> 
                   </div>                      
                <div className={styles.nav}>           
       <a href="https://luke-three.vercel.app/">
