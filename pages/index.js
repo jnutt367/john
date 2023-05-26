21563d7535ebd9c6b65446a50bcbf92aee96e30b
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-05-25 21:47:59 -0400

    Update index.js
    <iframe width="100%" height="315" src="https://www.youtube.com/embed/G-2e9mMf7E8" title="YouTube video player" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
                 
                             
  
-<iframe width="100%" height="250" src="https://www.youtube.com/embed/3-3xRwS4J3Q" title="YouTube video player" frameborder="5" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>  
+<iframe width="100%" height="315" src="https://www.youtube.com/embed/G-2e9mMf7E8" title="YouTube video player" frameborder="7" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> 
                   </div>                      
                <div className={styles.nav}>           
       <a href="https://luke-three.vercel.app/">
