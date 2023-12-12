978ed3f5547e3a0d853609eb8b64eb90930defe5
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-12-12 15:12:12 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1201,7 +1201,7 @@ Jesus said, “Feed my sheep. 18 Very truly I tell you, when you were younger yo
             </p>
           </div>
 
-   <div className={styles.iframe1}>
+   <div className={styles.iframe}>
            <iframe width="100%" height="315" src="https://www.youtube.com/embed/RUfh_wOsauk?autoplay=1&mute=1" title="YouTube video player" frameborder="5" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
   </div>
