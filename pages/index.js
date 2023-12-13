8fce0f53cd37f8b604b3ae03e2ad6f1fc9d31caf
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-12-13 11:06:48 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
         JOHN
         </h1>
                   <div className={styles.iframe1}>
-      <iframe width="100%" height="315" src="https://www.youtube.com/embed/8oz1JK19_B0?si=l9dPeVY7ekXIB78X&autoplay=1&mute=1" title="YouTube video player" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="100%" height="300" src="https://www.youtube.com/embed/8oz1JK19_B0?si=l9dPeVY7ekXIB78X&autoplay=1&mute=1" title="YouTube video player" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
  
   </div>   
                    <p className={styles.parables}>
@@ -1277,7 +1277,7 @@ Jesus said, “Feed my sheep. 18 Very truly I tell you, when you were younger yo
          
                </div>
     <div className={styles.iframe}>
-        <iframe width="100%" height="315" src="https://www.youtube.com/embed/CGbNw855ksw?autoplay=1&mute=1" title="YouTube video player" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> 
+        <iframe width="100%" height="300" src="https://www.youtube.com/embed/CGbNw855ksw?autoplay=1&mute=1" title="YouTube video player" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> 
     </div>    
 
         <div className={styles.grid}>
@@ -2340,7 +2340,7 @@ and turn, and I would heal them.’[a]
        PHILEMON
         </h1>
           <div className={styles.iframe1}>
-                           <iframe width="100%" height="315" src="https://www.youtube.com/embed/8oz1JK19_B0?si=WkSvaGXFDhW9N25W&autoplay=1&mute=1" title="YouTube video player" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                           <iframe width="100%" height="300" src="https://www.youtube.com/embed/8oz1JK19_B0?si=WkSvaGXFDhW9N25W&autoplay=1&mute=1" title="YouTube video player" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                                                            
 
                         </div>
