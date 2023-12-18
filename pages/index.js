3be3f350362dbb56af4dea48eb55f183a145cf4a
Jason Nutt (He/Him) Christian Developer/Creator <jnutt367@gmail.com>
2023-12-18 16:53:40 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,10 @@ export default function Home() {
      <link href="https://fonts.googleapis.com/css2?family=Rubik+Doodle+Shadow&display=swap" 
             rel="stylesheet"  
               />      
-                          
+
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital@1&family=Nanum+Brush+Script&display=swap" 
+              rel="stylesheet" 
+                />                
       </Head>
   
      
